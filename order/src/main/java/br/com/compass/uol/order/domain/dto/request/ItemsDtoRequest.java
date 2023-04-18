61b3a221a7df81ff6a394ecdd279279b69ad3425
JhonataCustodio <silvajhonata@live.com>
2023-04-18 20:09:31 -0300

    class construction and dto items

--- order/src/main/java/br/com/compass/uol/order/domain/dto/request/ItemsDtoRequest.java
+++ order/src/main/java/br/com/compass/uol/order/domain/dto/request/ItemsDtoRequest.java
@@ -1,4 +1,20 @@
 package br.com.compass.uol.order.domain.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemsDtoRequest {
+    private String name;
+    private String description;
+    private LocalDate creationDate;
+    private LocalDate expirationDate;
+    private Double amount;
 }
--- order/src/main/java/br/com/compass/uol/order/domain/entity/Items.java
+++ order/src/main/java/br/com/compass/uol/order/domain/entity/Items.java
@@ -1,4 +1,21 @@
 package br.com.compass.uol.order.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Items {
+    private ObjectId id;
+    private String name;
+    private String description;
+    private LocalDate creationDate;
+    private LocalDate expirationDate;
+    private Double amount;
 }
