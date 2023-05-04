1260f7ab1a3414b2b03b37edf6b2d7c87ab92671
JhonataCustodio <silvajhonata@live.com>
2023-05-04 11:16:37 -0300

    created ORDER_LIST for test controller

--- order/src/test/java/br/com/compass/uol/order/common/OrderConstants.java
+++ order/src/test/java/br/com/compass/uol/order/common/OrderConstants.java
@@ -1,13 +1,11 @@
 package br.com.compass.uol.order.common;
 
-import br.com.compass.uol.order.domain.dto.request.OrderDtoRequest;
 import br.com.compass.uol.order.domain.dto.response.ItemsDtoResponse;
 import br.com.compass.uol.order.domain.dto.response.OrderDtoResponse;
 import br.com.compass.uol.order.domain.entity.Items;
 import br.com.compass.uol.order.domain.entity.Order;
 import br.com.compass.uol.order.domain.enums.OrderStatus;
 import br.com.compass.uol.order.domain.enums.PaymentStatus;
-import br.com.compass.uol.order.factory.ItemsDtoResponseFactory;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -32,14 +30,22 @@ public class OrderConstants {
         setOrderStatus(OrderStatus.FINISHED);
         setPaymentStatus(PaymentStatus.APPROVED);
     }};
-//    public static final OrderDtoRequest CREATE_ORDER = new OrderDtoRequest(){{
-//        setCpf("12345678910");
-//        setItems(
-//                (Items) Collections.singletonList(new Items(2))
-//        );
-//        setAmount(44.8);
-//        setOrderStatus(OrderStatus.FINISHED);
-//        setPaymentStatus(PaymentStatus.APPROVED);
-//    }};
+    public static final List<OrderDtoResponse> ORDER_LIST = Arrays.asList(
+            new OrderDtoResponse() {{
+                setId(1);
+                setCpf("12345678910");
+                setItems(Collections.singletonList(new ItemsDtoResponse(
+                        4,
+                        "item name",
+                        "item description",
+                        LocalDate.of(2023, 4, 6),
+                        LocalDate.of(2025, 1, 1),
+                        30.0
+                )));
+                setAmount(44.8);
+                setOrderStatus(OrderStatus.FINISHED);
+                setPaymentStatus(PaymentStatus.APPROVED);
+            }}
+    );
 
 }
